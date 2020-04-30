24f0f240c96b17843b07d396174fc1c6c7893e38
fleetcoder <fleetcoder>
2020-04-30 23:16:44 +0000

    build fleet-ui

--- src/main.js
+++ src/main.js
@@ -10,13 +10,10 @@ import FileUpload from 'v-file-upload'
 import VueRouter from 'vue-router'
 import 'mint-ui/lib/style.css'
 import AudioRecorder from 'vue-audio-recorder'
-import PrismEditor from 'vue-prism-editor'
 Vue.use(MintUI)
 Vue.use(FileUpload)
 Vue.use(AudioRecorder)
 Vue.use(VueRouter)
-import "vue-prism-editor/dist/VuePrismEditor.css"
-Vue.component("prism-editor", PrismEditor)
 Vue.component(MUI.Radio.name, MUI.Radio)
 Vue.config.productionTip = false
 import VueResource from 'vue-resource'
@@ -30,4 +27,3 @@ window.MintUI = MintUI
 window.VueRouter = VueRouter
 window.FileUpload = FileUpload
 window.AudioRecorder = AudioRecorder
-window.PrismEditor = PrismEditor
